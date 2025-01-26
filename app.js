3203c8467b079f22b78d38c3583d02be063c7db5
Yacine Abed <yacine.abed@student.univ-temouchent.edu.dz>
2025-01-26 16:12:04 +0100

    Add mouse events
    created four divs and added the mouse events:
    mouseover: mouse hovering over element
    mouseleave: mouse leaving element
    dblclick: double click on element
    mousemove: moving mouse over element

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 const app = Vue.createApp({
     data() {
         return {
+            boxStyle: {
+                backgroundColor: 'cadetblue',
+            },
+            colors: [
+                "black",
+                "blue",
+                "red",
+                "purple",
+                "green",
+                "gray",
+                "orange"
+            ],
             showContent: true,
             chapter1: 'Introduction',
             content1_1: 'Reminder',
             content1_2: 'History',
-            content1_3: 'Basics'
+            content1_3: 'Basics',
+            eventText: 'Hover Here',
+            event2Text: 'No',
+            x_mov:0, y_mov:0,
         }
     },
     methods: {
         toggleChapter() {
             this.showContent = !this.showContent
         },
-        
+        handleHover(e, message) {
+            console.log(e);
+            if(e.type="mouseover") {
+                this.eventText = message;
+            }
+            if(e.type="mouseleave") {
+                this.eventText = message;
+            }
+        },
+        handleClick(e, message) {
+            if(e.type="click") {
+                this.event2Text = message;
+            } else {
+                this.event2Text = message;
+            }
+        },
+        handleDblclick(e) {
+            //pick a random color
+            this.boxStyle.backgroundColor = this.colors[Math.floor(Math.random() * this.colors.length)];;
+        },
+        handleMovement(e) {
+                this.x_mov = e.screenX;
+                this.y_mov = e.screenY;            
+        }
     }
 
 })
